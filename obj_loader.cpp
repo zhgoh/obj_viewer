57147e56544adeb7b015b385ab845c2dcb0716af
Goh Zi He <zihe.goh@gmail.com>
2024-05-09 10:00:56 +0800

    Add project files.

---
+++ obj_loader.cpp
@@ -0,0 +1,12 @@
+﻿// obj_loader.cpp : Defines the entry point for the application.
+//
+
+#include "obj_loader.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
